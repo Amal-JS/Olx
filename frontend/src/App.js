0ed765ddb6bbca818d98556af366171b76419193
amaljs <amaljs121@gmail.com>
2023-11-27 18:56:15 +0530

    Olx ( React , DRF )

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="text-8xl">
+       Heading
+      </header>
+    </div>
+  );
+}
+
+export default App;

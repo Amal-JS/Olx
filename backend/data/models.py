8baac6a7de5b2752d0f053f80e6bbebd7455511e
amaljs <amaljs121@gmail.com>
2023-11-30 14:31:03 +0530

    backend models,views created

--- backend/data/models.py
+++ backend/data/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CustomUser(User):
+    phone = models.CharField(max_length=10)
+  
+    def __str__(self) -> str:
+        return f"{self.username} {self.phone}"
+
+class Post(models.Model):
+    user = models.ForeignKey(CustomUser,on_delete=models.CASCADE)
+    name = models.CharField(max_length=40)
+    brand =models.CharField(max_length=40)
+    description = models.TextField()
+    place = models.CharField(max_length=100)
+    timespan = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='post_images',null=True,blank=True)
+
+    def __str__(self) -> str:
+        return f"{self.user} : {self.name}"
+    
+
+
--- backend/data/views.py
+++ backend/data/views.py
@@ -1,3 +1,68 @@
-from django.shortcuts import render
 
-# Create your views here.
+from . models import Post,CustomUser
+from . serializers import PostSerializer,UserSerializer
+from rest_framework import viewsets 
+from rest_framework.response import Response
+
+
+class PostViewSets(viewsets.ModelViewSet):
+    queryset = Post.objects.all()  
+    serializer_class = PostSerializer
+
+
+    def create(self,request):
+
+        print(request, request.data)
+        serializer = PostSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+       
+            msg='Data saved'
+            return Response({'msg':msg})
+        return Response(serializer.errors,status=400)
+    
+    def retrieve(self,request,pk=None):
+            
+            if pk is not None:
+                  
+                  try:
+                    queryset = Post.objects.get(pk=pk)
+                    serializer = self.get_serializer(queryset)
+                  except Post.DoesNotExist:
+                        return Response({'msg':'Provide a valid id'})
+                  return Response(serializer.data)
+           
+
+    
+
+
+        
+class UserViewSets(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()  
+    serializer_class = UserSerializer
+
+
+    def create(self,request):
+
+        print(request, request.data)
+        serializer = UserSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+       
+            
+            return Response({'userCreation':True})
+        return Response(serializer.errors,status=400)
+    
+    def retrieve(self,request):
+            
+            data = request.data     
+            try:
+                user = CustomUser.objects.get(username=data.username)
+
+                if user.password == data.password:
+                    return Response({'validUser':True})
+            except CustomUser.DoesNotExist:
+                return Response({'validUser':False})
+            
+            return Response({'validUser':False})
+    

7e2ebef5a621e1a6154e1145b767f5d067db3923
amaljs <amaljs121@gmail.com>
2023-12-03 20:42:33 +0530

    comments added fully

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,15 @@ from django.conf.urls.static import static
 from data import views
 from rest_framework import routers
 
+
 router=routers.DefaultRouter()
+
+#when you register a viewset with a router, it automatically generates URL patterns for various actions 
+#(e.g., list, create, retrieve, update, delete). The basename is used to name these URL patterns.
+# basename is used to name these URLs.
+# In a view
+#reverse('posts-list')   # Returns '/posts/' (the list endpoint)
+
 router.register('posts',views.PostViewSets,basename='posts') 
 router.register('user',views.UserViewSets,basename='user') 
 
@@ -14,4 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include(router.urls))
 ]
+
+#serve media files 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
